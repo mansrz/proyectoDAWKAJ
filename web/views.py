bc4f07584305ba364c9e3af12ba10e9649f3044b
mansrz <mansrz@gmail.com>
2016-01-07 12:56:58 -0500

    Add template and viewfor show user

--- web/views.py
+++ web/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from web.models import *
 
 # Create your views here.
 
 def home(request):
-    return render(request,'index.html',{})
+    users = User.objects.all()
+    return render(request,'user.html',{'users':users})
