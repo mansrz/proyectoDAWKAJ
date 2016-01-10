c414d9ebbac7461b9c2b698616d8cf92f8928b5f
mansrz <mansrz@gmail.com>
2016-01-10 12:35:50 -0500

    changes in url in project config

--- daw/urls.py
+++ daw/urls.py
@@ -7,6 +7,5 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'$', include('web.urls')),
-    
+    url(r'^', include('web.urls')),
 ]
